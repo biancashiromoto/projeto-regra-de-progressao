e6dbc500f3f69ef17d60072e5ff970f4be27e53f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-20 13:27:53 -0300

    feat: Implement total weight validation

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,6 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,15 +17,22 @@ public class App {
     int activities = scanner.nextInt();
     scanner.nextLine();
 
+    int totalWeights = 0;
+
     for (int i = 1; i <= activities; i += 1) {
       System.out.printf("Digite o nome da atividade %s: ", i);
       String name = scanner.nextLine();
       System.out.printf("Digite o peso da atividade %s: ", i);
       int weight = scanner.nextInt();
+      totalWeights += weight;
       scanner.nextLine();
       System.out.printf("Digite a nota obtida para %s: ", name);
       int grade = scanner.nextInt();
       scanner.nextLine();
+    }
+
+    if (totalWeights != 100) {
+      System.out.println("A soma dos pesos é diferente de 100!");
     }
   }
 }
